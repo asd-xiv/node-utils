5ef359284bdd9a92aad4303c0a0824b5e7cf07c9
Andrei Dumitrescu <5057797+andreidmt@users.noreply.github.com>
2026-06-14 01:21:48 +0200

    build: polish dev configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,24 @@
-import { tsNodeConfig, commonIgnores } from "@asd14/eslint-config/typescript"
+import { tsConfig, devConfig, commonIgnores } from "@asd14/eslint-config/ts"
 
 const SRC_FILES = ["src/**/*.ts"]
 const TEST_FILES = ["src/**/*.test.ts"]
+const TST_FILES = ["src/**/*.tst.ts"]
 const DEV_FILES = ["eslint.config.js"]
 
 /** @type {import("eslint").Linter.Config[]} */
 export default [
+  { ignores: commonIgnores },
+  { files: SRC_FILES, ...tsConfig },
   {
-    ignores: [...commonIgnores],
-  },
-  {
-    ...tsNodeConfig,
-    files: [...SRC_FILES, ...DEV_FILES, ...TEST_FILES],
+    files: [...TEST_FILES, ...TST_FILES, ...DEV_FILES],
+    ...tsConfig,
+    ...devConfig,
+    rules: {
+      ...tsConfig.rules,
+      ...devConfig.rules,
+
+      // `node:test` describe/test return promises that are handled by the test runner
+      "@typescript-eslint/no-floating-promises": "off",
+    },
   },
 ]
